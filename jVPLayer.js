2ece72f5d689ba89eab89ac68c57157e57062e03
Bogdan <nmunchkin@ya.ru>
2015-08-16 18:46:43 +0500

    Base funtions implimented

---
+++ jVPLayer.js
@@ -0,0 +1,81 @@
+
+function jVPlayer(){
+  var player = this
+    , prefix
+    , include = {};
+
+  function createElement(tn){
+    return document.createElement(tn);
+  }
+
+  var rootLayer = createElement('div')
+    , v = createElement('video');
+
+  rootLayer.classList.add('jvp-root-layer');
+  rootLayer.appendChild(v);
+
+
+  // 
+  prefix = (function (){
+    var scripts = document.getElementsByTagName('script');
+    for (var i = 0; i < scripts.length; ++i){
+      if(scripts[i].src.indexOf('jVPlayer.js') > -1){
+        var tmp = scripts[i].src.replace(window.location, '')
+        tmp = tmp.split('/');
+        tmp.pop();
+        return '/'+tmp.join('/')+'/';
+      }
+    }
+  })();
+
+  var includeLDR = new XMLHttpRequest();
+  includeLDR.open('POST', prefix+'player/include.js', false);
+  includeLDR.send();
+  var includeSRC = includeLDR.responseText
+    , Include = (new Function('module', includeSRC+'return module.exports;'))({});
+
+  include = new Include(prefix);
+
+
+  this.init = function(param){
+    window.addEventListener('load', function(e){
+      init.call(player, param);
+    });
+  }
+
+  function init(param){
+    var rootRoot = document.querySelector(param.selector);
+    try{
+      rootRoot.appendChild(rootLayer);
+    } catch(e){
+      console.error('Selector was not specified');
+      return;
+    }
+
+    param.size = param.size || 'auto';
+    v.controls = param.controls || false;
+
+    v.src = param.src
+    v.addEventListener('loadstart', function(e){
+      if(param.size === 'auto'){
+        rootLayer.style.width = v.clientWidth+'px';
+        rootLayer.style.height = v.clientHeight+'px';
+      } else if(param.size.indexOf('%') > -1){
+        rootLayer.style.width = param.size;
+        rootLayer.style.height = param.size;
+      } else if(param.size.indexOf('x') > -1){
+        var w = param.size.split('x')[0];
+        var h = param.size.split('x')[1];
+        rootLayer.style.width = w+'px';
+        rootLayer.style.height = h+'px';
+      }else{
+        rootLayer.style.width = v.clientWidth+'px';
+        rootLayer.style.height = v.clientHeight+'px';
+      }
+      v.style.width = '100%';
+      v.style.height = '100%';
+    })
+
+      
+  }
+}
---
+++ player/include.js
@@ -0,0 +1,11 @@
+
+function Include(prefix){
+
+}
+
+Include.prototype = {
+  constructor: Include,
+
+}
+
+module.exports = Include;
