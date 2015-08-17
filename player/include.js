dcc72aa2716055585117cf25c02f745bcdf69b15
Bogdan <nmunchkin@ya.ru>
2015-08-17 18:05:53 +0500

    Inlude functional

--- player/include.js
+++ player/include.js
@@ -1,11 +1,39 @@
+var basPrefix
+  , cnfPrefix
+  , extPrefix
+  , lclPrefix
+  , plrPrefix
+  , thmPrefix;
 
-function Include(prefix){
+function makeExtension(MDLSrc){
+  var fullSrc = MDLSrc + 'return module.exports';
+  return (new Function('module', fullSrc))({exports:{}});
+}
 
+function Include(pref){
+  basPrefix = pref;
+  cnfPrefix = 'config/';
+  extPrefix = 'extensions/';
+  lclPrefix = 'locales/';
+  plrPrefix = 'player/';
+  thmPrefix = 'themes/';
 }
 
 Include.prototype = {
   constructor: Include,
-
+  plainText: function (path){
+    var textLdr = new XMLHttpRequest();
+    textLdr.open('POST', path, false);
+    return textLdr.responseText;
+  },
+  module: function(name){
+    var src = this.plainText(basPrefix+plrPrefix+name+'/'+name+'.js');
+    return makeExtension(src);
+  },
+  JSON: function(path){
+    var JSONSrc = this.plainText(path);
+    return JSON.parse(JSONSrc);
+  }
 }
 
 module.exports = Include;
